2152f9109617a13af9c8fe9d5fde84d676f917f5
Loïc Souêtre <loic.souetre@sfr.fr>
2024-05-11 16:27:00 +0200

    Ajout d'un fond pour la num de la manche

--- src/pages/Game/styles.jsx
+++ src/pages/Game/styles.jsx
@@ -1,10 +1,15 @@
 import styled from 'styled-components';
+import colors from '../../utils/style/colors.js';
 
 export const RoundContainer = styled.div`
     font-size: 20px;
     position: absolute;
     top: 0;
-    margin-top: 70px;
+    margin-top: 100px;
+    background-color: ${colors.secondary};
+    padding: 5px 20px;
+    border-radius: 10px;
+    color: white;
 
     @media screen and (max-width: 425px) {
         font-size: 16px;
