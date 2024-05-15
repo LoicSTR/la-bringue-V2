f353937fb7ddcf2b62e912e23a9b7d46bc1824f2
Loïc Souêtre <loic.souetre@sfr.fr>
2024-05-15 15:53:49 +0200

    Deploy 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import GlobalStyle from './utils/style/GlobalStyle';
 import Home from './pages/Home';
 import Players from './pages/Players';
@@ -8,14 +9,14 @@ import Game from './pages/Game';
 function App() {
     return (
         <React.StrictMode>
-            <Router>
+            <HashRouter>
                 <GlobalStyle />
                 <Routes>
                     <Route path="/" element={<Home />} />
                     <Route path="/players" element={<Players />} />
                     <Route path="/game" element={<Game />} />
                 </Routes>
-            </Router>
+            </HashRouter>
         </React.StrictMode>
     );
 }
