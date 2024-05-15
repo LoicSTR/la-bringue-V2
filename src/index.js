7677d9b06e1cc3b3b0b07d4ebb02e93cf0984e47
Loïc Souêtre <loic.souetre@sfr.fr>
2024-05-15 15:44:20 +0200

    Ajout de HashRouter dans App

--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
-import { HashRouter } from 'react-router-dom';
 
 const root = createRoot(document.getElementById('root'));
-root.render(
-    <HashRouter>
-        <App />
-    </HashRouter>
-);
+root.render(<App />);
